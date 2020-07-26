d05ffa15693f083c4f8baa2e6a8f47417a82f264
julianamonr03 <1967@holbertonschool.com>
2020-07-26 22:44:31 +0000

    Bug fix

--- printf.c
+++ printf.c
@@ -10,8 +10,7 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 
-	int i = 0;
-	int j = 0;
+	int i = 0, j = 0, cont = 0;
 	char *str;
 	char tmp[2];
 	char buf[2048];
@@ -46,7 +45,7 @@ int _printf(const char *format, ...)
 				j += _strlen(str) - 1;
 				if (format[i] == 'c' && str[0] == '\0')
 				{
-					j++;
+					cont++;
 				}
 			}
 			else
@@ -61,7 +60,7 @@ int _printf(const char *format, ...)
 	buf[j] = '\0';
 	write(1, buf, j);
 	va_end(args);
-	return (j);
+	return (cont + j);
 }
 /**
  * *_strcpy - copies string pointed to by src
