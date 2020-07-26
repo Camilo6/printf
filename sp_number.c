e1763f7b7a0c8fbf5715d0302f8129b3dbe71265
julianamonr03 <1967@holbertonschool.com>
2020-07-26 21:08:54 +0000

    Return malloc

--- sp_number.c
+++ sp_number.c
@@ -14,6 +14,7 @@ char *sp_number(va_list args)
 	if (buf == NULL)
 	{
 		free(buf);
+		return (NULL);
 	}
 	if (n < 0)
 	{
@@ -40,6 +41,5 @@ char *sp_number(va_list args)
 			nm++;
 		}
 	}
-
 	return (buf);
 }
